cd2037c1c9b872279002fd9a76645958d03faed7
Sergey Ganin <mcheguevara2@gmaiil.com>
2019-10-18 00:08:21 +0300

    fix

--- pointAbstraction/pointAbstraction.js
+++ pointAbstraction/pointAbstraction.js
@@ -1,14 +1,16 @@
-const makePoint = () => {};
+const makePoint = (x, y) => [x, y];
 
-const getX = () => {};
+const getX = ([x]) => x;
 
-const getY = () => {};
+const getY = ([, y]) => y;
 
-const toString = () => {};
+const toString = vector => JSON.stringify(vector);
 
-const getDistance = () => {};
+const getDistance = ([x1, y1], [x2, y2]) => {
+  return Math.sqrt(x2 - x1) + Math.sqrt(y2 - y1);
+};
 
-const getQuadrant = () => {};
+const getQuadrant = vector => vector.map();
 
 module.exports = {
   makePoint,
