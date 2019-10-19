20a8659b3fd5ca4d46bd39c047299d4921e23978
Gic0 <g.zdanovich@onpoint.ru>
2019-10-20 00:51:56 +0300

    complete dz

--- pointAbstraction/pointAbstraction.js
+++ pointAbstraction/pointAbstraction.js
@@ -1,14 +1,24 @@
-const makePoint = () => {};
+const makePoint = (x, y) => ({ x, y });
 
-const getX = () => {};
+const getX = ({ x }) => x;
 
-const getY = () => {};
+const getY = ({ y }) => y;
 
-const toString = () => {};
+const toString = ({ x, y }) => `[${x}, ${y}]`;
+// sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
+const getDistance = (dote1, dote2) =>
+  Math.sqrt((dote2.x - dote1.x) ** 2 + (dote2.y - dote1.y) ** 2);
 
-const getDistance = () => {};
-
-const getQuadrant = () => {};
+const getQuadrant = ({ x, y }) =>
+  x !== 0 && y !== 0
+    ? x > 0 && y > 0
+      ? 1
+      : x < 0 && y > 0
+      ? 2
+      : x < 0 && y < 0
+      ? 3
+      : 4
+    : null;
 
 module.exports = {
   makePoint,
--- pseudoRouter/pseudoRouter.js
+++ pseudoRouter/pseudoRouter.js
@@ -1,3 +1,8 @@
-// const Router = function() {};
+const Router = function() {
+  this.bind = (path, type, cb) => (this[path] = { [type]: cb });
+
+  this.runRequest = (path, type) =>
+    this[path][type] ? this[path][type]() : "Error 404: Not Found";
+};
 
 module.exports = Router;
